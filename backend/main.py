d5f6bb83aa36329abce6d9d6c73348db4e13289a
Nikhil Mishra <nikhilmishra2003kampil@gmail.com>
2026-06-16 01:41:23 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_methods=["*"]
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 database_models.Base.metadata.create_all(bind=engine)
